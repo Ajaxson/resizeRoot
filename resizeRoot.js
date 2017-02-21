497976466a3f3d2fe2bacee5fcfe9b3e29ff0f4b
Ajaxson <2287956894@qq.com>
2017-02-21 14:44:27 +0800

    Update resizeRoot.js

--- resizeRoot.js
+++ resizeRoot.js
@@ -28,54 +28,54 @@
         // 设计稿大小
         var p = p;
         var dpr = window.devicePixelRatio;
-        if(initialScale >= 1){   
-                // 抽取某易计算方法
-                var psWidth = p / 100;  
-                var wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width : window.innerWidth : window.innerWidth, wDpr, wFsize;
-                var wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ? screen.height : window.innerHeight : window.innerHeight;
-                if (window.devicePixelRatio) {
-                    wDpr = window.devicePixelRatio;
-                } else {
-                    wDpr = isIOS ? wWidth > 818 ? 3 : wWidth > 480 ? 2 : 1 : 1;
-                }
-                if(isIOS) {
-                    wWidth = screen.width;
-                    wHeight = screen.height;
-                }
-                // if(window.orientation==90||window.orientation==-90){
-                //     wWidth = wHeight;
-                // }else if((window.orientation==180||window.orientation==0)){
-                // }
-                if(wWidth > wHeight && isOrientation == true){
-                    wWidth = wHeight;
-                }
-                wFsize = wWidth > isMax ? isMax / psWidth : wWidth / psWidth;
-                window.screenWidth_ = wWidth;
-                if(isYIXIN || is2345 || ishaosou || isSogou || isLiebao || isGnbr){//YIXIN 和 2345 这里有个刚调用系统浏览器时候的bug，需要一点延迟来获取
-                    setTimeout(function(){
-                        wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width : window.innerWidth : window.innerWidth;
-                        wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ? screen.height : window.innerHeight : window.innerHeight;
-                        wFsize = wWidth > isMax ? isMax / psWidth : wWidth / psWidth;
-                        // document.getElementsByTagName('html')[0].dataset.dpr = wDpr;
-                        document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
-                    },500);
-                }else{
-                    document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
-                }
-        }else{
-        // 当页面字体有小于极限字体时(去打设计狮咯)，即 viewport的 initial-scale  设置成0.5
-            if(isYIXIN || is2345 || ishaosou || isSogou || isLiebao || isGnbr){//YIXIN 和 2345 这里有个刚调用系统浏览器时候的bug，需要一点延迟来获取
-                setTimeout(function(){
-                    var rootWi = document.documentElement.clientWidth;
-                    var rootHi = document.documentElement.clientHeight;
-                    if(rootWi > rootHi && isOrientation == true){
-                        rootWi = rootHi;
-                    }
-                    rootWi = rootWi > isMax * (dpr/1) ? isMax * (dpr/1) : rootWi;
-                    var rootSize = rootWi / p * 100;
-                    document.documentElement.style.fontSize = rootSize + "px";
-                },500);
-            }else{
+//         if(initialScale >= 1){   
+//                 // 抽取某易计算方法
+//                 var psWidth = p / 100;  
+//                 var wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width : window.innerWidth : window.innerWidth, wDpr, wFsize;
+//                 var wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ? screen.height : window.innerHeight : window.innerHeight;
+//                 if (window.devicePixelRatio) {
+//                     wDpr = window.devicePixelRatio;
+//                 } else {
+//                     wDpr = isIOS ? wWidth > 818 ? 3 : wWidth > 480 ? 2 : 1 : 1;
+//                 }
+//                 if(isIOS) {
+//                     wWidth = screen.width;
+//                     wHeight = screen.height;
+//                 }
+//                 // if(window.orientation==90||window.orientation==-90){
+//                 //     wWidth = wHeight;
+//                 // }else if((window.orientation==180||window.orientation==0)){
+//                 // }
+//                 if(wWidth > wHeight && isOrientation == true){
+//                     wWidth = wHeight;
+//                 }
+//                 wFsize = wWidth > isMax ? isMax / psWidth : wWidth / psWidth;
+//                 window.screenWidth_ = wWidth;
+//                 if(isYIXIN || is2345 || ishaosou || isSogou || isLiebao || isGnbr){//YIXIN 和 2345 这里有个刚调用系统浏览器时候的bug，需要一点延迟来获取
+//                     setTimeout(function(){
+//                         wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width : window.innerWidth : window.innerWidth;
+//                         wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ? screen.height : window.innerHeight : window.innerHeight;
+//                         wFsize = wWidth > isMax ? isMax / psWidth : wWidth / psWidth;
+//                         // document.getElementsByTagName('html')[0].dataset.dpr = wDpr;
+//                         document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
+//                     },500);
+//                 }else{
+//                     document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
+//                 }
+//         }else{
+//         // 当页面字体有小于极限字体时(去打设计狮咯)，即 viewport的 initial-scale  设置成0.5
+//             if(isYIXIN || is2345 || ishaosou || isSogou || isLiebao || isGnbr){//YIXIN 和 2345 这里有个刚调用系统浏览器时候的bug，需要一点延迟来获取
+//                 setTimeout(function(){
+//                     var rootWi = document.documentElement.clientWidth;
+//                     var rootHi = document.documentElement.clientHeight;
+//                     if(rootWi > rootHi && isOrientation == true){
+//                         rootWi = rootHi;
+//                     }
+//                     rootWi = rootWi > isMax * (dpr/1) ? isMax * (dpr/1) : rootWi;
+//                     var rootSize = rootWi / p * 100;
+//                     document.documentElement.style.fontSize = rootSize + "px";
+//                 },500);
+//             }else{
                 var rootWi = document.documentElement.clientWidth;
                 var rootHi = document.documentElement.clientHeight;
                 if(rootWi > rootHi && isOrientation == true){
@@ -84,8 +84,8 @@
                 rootWi = rootWi > isMax * (dpr/1) ? isMax * (dpr/1) : rootWi;
                 var rootSize = rootWi / p * 100;
                 document.documentElement.style.fontSize = rootSize + "px";
-            }
-        }
+//             }
+//         }
     }
     resizeRoot(ps);
     window.onresize = function(){
